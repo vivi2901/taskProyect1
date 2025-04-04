6e6c787e53de99f2b3bdd006c02b1ce1dc33cc17
vivi2901 <202000200@est.umss.edu>
2025-04-03 20:36:23 -0400

    eslintPrettier

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 //import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY } from '@env';
 
